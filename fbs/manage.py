7de195b3d06c56cee8caa234beabc82f89e56173
Amine Kidane <100053678@ku.ac.ae>
2023-03-18 13:15:58 +0400

    manage.py to a new branch

--- fbs/manage.py
+++ fbs/manage.py
@@ -16,6 +16,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-print()
+print("this sould show in the new branch")
 if __name__ == "__main__":
     main()
