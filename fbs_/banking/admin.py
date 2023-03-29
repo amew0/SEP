90bf93a736450d7697268872a17d3602f219ed7f
Amine Kidane <100053678@ku.ac.ae>
2023-03-29 23:57:23 +0400

    Removed fbsflutter fbs_ and gitignore renamed

--- fbs_/banking/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(CreditCardDetail)
-admin.site.register(User)
-admin.site.register(Balance)
-admin.site.register(Allowance)
-admin.site.register(Bill)
--- fbsflutter/android/app/src/main/kotlin/com/example/fbsflutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fbsflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
